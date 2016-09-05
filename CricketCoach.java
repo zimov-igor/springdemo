a169474bf6935a6e948b21172c8a32a932d75f4f
zimov-igor <igorzim30@gmail.com>
2016-09-05 23:40:57 +0300

    my second comit(seter)

---
+++ CricketCoach.java
@@ -0,0 +1,27 @@
+package com.iuv2code.springdemo;
+
+public class CricketCoach implements Coach {
+
+	private FortuneService fortuneService;
+	
+	public CricketCoach() {
+		super();
+		System.out.println("CricketCoach: inside no-arg constructor");
+	}
+	
+	public void setFortuneService(FortuneService fortuneService) {
+		System.out.println("CricketCoach: inside setter method - fortuneService");
+		this.fortuneService = fortuneService;
+	}
+
+	@Override
+	public String getDailyWorkout() {
+		return "Practice fast bowling for 15 minutes";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		return fortuneService.getFortune();
+	}
+
+}
